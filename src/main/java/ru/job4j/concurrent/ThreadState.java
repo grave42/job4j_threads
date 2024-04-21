fcadae472d7a19a00a555f8e2be324f1595c9a59
filipp.kucherov <filipp.kucerov@r1.team>
2024-04-21 18:37:56 +0700

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,31 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                }
+        );
+        System.out.println(first.getState());
+        System.out.println(first.getName());
+        System.out.println(second.getState());
+        System.out.println(second.getName());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getState());
+        }
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getState());
+        }
+        System.out.println(first.getState());
+        System.out.println(second.getState());
+        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
+            System.out.println("Thread is terminated");
+        }
+    }
+}
