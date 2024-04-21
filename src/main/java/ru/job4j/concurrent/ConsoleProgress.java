c4d1b2fc355514388ded6dfc8d24c7b69fdd47ca
filipp.kucherov <filipp.kucerov@r1.team>
2024-04-21 19:52:06 +0700

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/' };
+        int currentIndex = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r load: " + process[currentIndex]);
+            currentIndex = (currentIndex + 1) % process.length;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+}
+
