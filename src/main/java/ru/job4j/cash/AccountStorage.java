0e5ca21ae93a12b3ed4c9bcf6c87c0f6c7417414
filipp.kucherov <filipp.kucerov@r1.team>
2024-05-02 10:04:47 +0700

    3. Денежные переводы AccountStorage [#1104 #493937] fix

--- src/main/java/ru/job4j/cash/AccountStorage.java
+++ src/main/java/ru/job4j/cash/AccountStorage.java
@@ -6,22 +6,16 @@ import java.util.Optional;
 public class AccountStorage {
     private final HashMap<Integer, Account> accounts = new HashMap<>();
 
-    public synchronized void add(Account account) {
-        accounts.put(account.id(), account);
+    public synchronized boolean add(Account account) {
+        return accounts.putIfAbsent(account.id(), account) == null;
     }
 
-    public synchronized void update(Account account) {
-        Optional<Account> accToUpdate = getById(account.id());
-        if (accToUpdate.isPresent()) {
-            accounts.put(account.id(), account);
-        }
+    public synchronized boolean update(Account account) {
+        return accounts.replace(account.id(), account) != null;
     }
 
     public synchronized void delete(int id) {
-        Optional<Account> accToDelete = getById(id);
-        if (accToDelete.isPresent()) {
-            accounts.remove(id);
-        }
+        accounts.remove(id);
     }
 
     public synchronized Optional<Account> getById(int id) {
@@ -31,7 +25,7 @@ public class AccountStorage {
     public synchronized void transfer(int fromId, int toId, int amount) {
         Optional<Account> from = getById(fromId);
         Optional<Account> to = getById(toId);
-        if (from.isPresent() && to.isPresent()) {
+        if (from.isPresent() && to.isPresent() && from.get().amount() >= amount) {
             update(new Account(fromId, from.get().amount() - amount));
             update(new Account(toId, to.get().amount() + amount));
         }
