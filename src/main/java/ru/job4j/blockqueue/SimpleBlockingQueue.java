d68dade5a3023daa5c306ccaf6aa0f59285775c8
filipp.kucherov <filipp.kucerov@r1.team>
2024-05-04 17:05:12 +0700

    1. Реализовать шаблон Producer Consumer. [#1098]

---
+++ src/main/java/ru/job4j/blockqueue/SimpleBlockingQueue.java
@@ -0,0 +1,44 @@
+package ru.job4j.blockqueue;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+
+    public int getMaxQueueSize() {
+        return maxQueueSize;
+    }
+
+    private final int maxQueueSize;
+
+    public SimpleBlockingQueue(int maxQueueSize) {
+        this.maxQueueSize = maxQueueSize;
+    }
+
+    public synchronized void offer(T value) throws InterruptedException {
+        while (queue.size() >= maxQueueSize) {
+            wait();
+        }
+        queue.offer(value);
+        notifyAll();
+    }
+
+    public synchronized T poll() throws InterruptedException {
+        while (queue.isEmpty()) {
+            wait();
+        }
+        notifyAll();
+        return queue.poll();
+    }
+
+    public synchronized boolean isEmpty() {
+        return queue.isEmpty();
+    }
+}
---
+++ src/test/java/ru/job4j/blockqueue/SimpleBlockingQueueTest.java
@@ -0,0 +1,45 @@
+package ru.job4j.blockqueue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SimpleBlockingQueueTest {
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        Thread producer = new Thread(
+                () -> {
+                    for (int i = 0; i < queue.getMaxQueueSize(); i++) {
+                        try {
+                            queue.offer(i);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).containsExactly(0, 1, 2, 3, 4);
+    }
+}
